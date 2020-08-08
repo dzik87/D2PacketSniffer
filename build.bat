a94d4411b0a03b85718bf71ab2892a5df2623585
dzik <dzik87@gmail.com>
2020-08-08 08:24:41 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -3,8 +3,8 @@ if not exist "%cd%\WpdPack\" (
 	echo.
 	echo ------------ Downloading WinPcap 4.1.2 Developer's Pack ------------
 	echo.
-	powershell -Command "Invoke-WebRequest https://www.winpcap.org/install/bin/WpdPack_4_1_2.zip  -OutFile WpdPack_4_1_2.zip
-	powershell Expand-Archive WpdPack_4_1_2.zip -destinationpath %cd%
+	powershell -Command "Invoke-WebRequest https://www.winpcap.org/install/bin/WpdPack_4_1_2.zip  -OutFile WpdPack_4_1_2.zip 
+	powershell Expand-Archive WpdPack_4_1_2.zip -destinationpath %cd% 
 	DEL /F /S /Q /A *.zip
 )
 echo.
